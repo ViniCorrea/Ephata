651fe51c0d42b789c9d6e4ca0822389037b001b8
Vinicius Correa <vinicius.correa@thomsonreuters.com>
2020-12-03 20:08:12 -0300

    Init with GraphQL

---
+++ src/Rol.Api/Data/RolDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Rol.Api.Data.Entities;
+
+namespace Rol.Api.Data
+{
+    public class RolDbContext : DbContext
+    {
+        public RolDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        private DbSet<Member> Members { get; set; }
+    }
+}
