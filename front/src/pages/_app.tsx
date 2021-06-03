0b52121d5256cb8699f986190d58926e6df58972
Thamyres Guedes <thataguedis@gmail.com>
2021-06-03 15:47:29 -0300

    Applyimg prettier patterns

--- front/src/pages/_app.tsx
+++ front/src/pages/_app.tsx
@@ -1,5 +1,5 @@
-import 'antd/dist/antd.css';
-import '../styles/globals.css';
+import "antd/dist/antd.css";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
--- front/src/pages/index.jsx
+++ front/src/pages/index.jsx
@@ -1,20 +1,20 @@
-import React from 'react';
-import Layout from '../components/layout';
+import React from "react";
+import Layout from "../components/layout";
 
-import { Empty } from 'antd';
+import { Empty } from "antd";
 
 const routes = [
   {
-    path: 'index',
-    breadcrumbName: 'First-level Menu',
+    path: "index",
+    breadcrumbName: "First-level Menu",
   },
   {
-    path: 'first',
-    breadcrumbName: 'Second-level Menu',
+    path: "first",
+    breadcrumbName: "Second-level Menu",
   },
   {
-    path: 'second',
-    breadcrumbName: 'Third-level Menu',
+    path: "second",
+    breadcrumbName: "Third-level Menu",
   },
 ];
 
