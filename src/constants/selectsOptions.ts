84e6b30861b9fc9f96a3a4107822c77f36481a94
Thamyres Guedes <thataguedis@gmail.com>
2021-08-15 16:13:09 -0300

    🎨 Configuring local api, mocking data and defining patterns

---
+++ src/constants/selectsOptions.ts
@@ -0,0 +1,115 @@
+export const RESPOSABILITIES = [
+  {
+    label: "Membro",
+    value: "member",
+  },
+  {
+    label: "Congregado",
+    value: "congregated",
+  },
+  {
+    label: "Auxiliar de Trabalho",
+    value: "workassistant",
+  },
+  {
+    label: "Diácono",
+    value: "deacon",
+  },
+  {
+    label: "Diáconisa",
+    value: "deaconess",
+  },
+  {
+    label: "Presbítero",
+    value: "elder",
+  },
+  {
+    label: "Presbítera",
+    value: "presbyter",
+  },
+  {
+    label: "Evangelista",
+    value: "evangelist",
+  },
+  {
+    label: "Missionária",
+    value: "missionary",
+  },
+  {
+    label: "Pastor",
+    value: "pastor",
+  },
+];
+
+export const MARITAL_STATUS = [
+  {
+    label: "Casado",
+    value: "married",
+  },
+  {
+    label: "Solteiro",
+    value: "single",
+  },
+  {
+    label: "Viúvo",
+    value: "widower",
+  },
+  {
+    label: "Divorciado",
+    value: "divorced",
+  },
+  {
+    label: "Separado",
+    value: "Separate",
+  },
+  {
+    label: "Amasiado",
+    value: "cohabitating ",
+  },
+];
+
+export const CHURCH = [
+  {
+    label: "Sede",
+    value: "headOffice",
+  },
+  {
+    label: "Vieiras",
+    value: "vieiras",
+  },
+  {
+    label: "Rosário da Limeira",
+    value: "rasarioDaLimeira",
+  },
+  {
+    label: "Colety",
+    value: "colety",
+  },
+  {
+    label: "Corrego dos Maias",
+    value: "corregoDosMaias",
+  },
+  {
+    label: "Queirozes",
+    value: "queirozes",
+  },
+];
+
+export const PUBLIC_PLACE = [
+  {
+    label: "Rua",
+    value: "street",
+  },
+  {
+    label: "Avenida",
+    value: "avenue",
+  },
+  {
+    label: "Praça",
+    value: "square",
+  },
+  {
+    label: "Vila",
+    value: "village",
+  },
+];
--- src/pages/members/index.tsx
+++ src/pages/members/index.tsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
-import { Empty, Table } from "antd";
-import data, { columns, routes } from "./data";
+import { Alert, Empty, Table } from "antd";
+import { columns, routes } from "../../constants/pages/members";
+import { membersList } from "../../mocks/members";
 import Layout from "../../components/layout";
 
 const MembersPage = () => {
+  // Local states
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
+
   const router = useRouter();
 
   // Creating action buttons
@@ -15,9 +20,24 @@ const MembersPage = () => {
     },
   ];
 
+  useEffect(() => {
+    fetch("/api/members")
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          setError(response.statusText);
+        }
+      })
+      .then((result) => setData(result));
+  }, []);
+
   return (
     <Layout breadcrumb={{ routes: routes }} title="Membros" buttons={buttons}>
-      {data.length > 0 ? (
+      {error && (
+        <Alert message="Error" description={error} type="error" showIcon />
+      )}
+      {membersList().length > 0 ? (
         <Table columns={columns} dataSource={data} />
       ) : (
         <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} />
